aad2baebd210bdea8cfe92d337d43db54d310f05
Hasan Shahriar <7091945+hsleonis@users.noreply.github.com>
2022-02-01 12:27:34 +0100

    Update settings.py

--- bamboo_hr/app/settings.py
+++ bamboo_hr/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b4yqu5!0a5qst#&8@$o4q+d@-ozs0v4e+=+%ys0h5_t9$9*wk6'
+SECRET_KEY = 'mmmmm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
